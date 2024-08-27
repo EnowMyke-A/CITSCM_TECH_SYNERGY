1e07b750ad22552391c564ed85a8726d33a4518b
fabriceMokfembam <fabricemokfembam@.com>
2024-08-27 11:02:57 -0700

    trying to do finishing touches

--- src/_creatives/pages/home/components/ProductList.tsx
+++ src/_creatives/pages/home/components/ProductList.tsx
@@ -1,13 +1,20 @@
 import { IonIcon } from '@ionic/react'
 import { chevronForward, play } from 'ionicons/icons'
 import React from 'react'
+import { useIonRouter } from "@ionic/react";
 
-function ProductList({arts}:any) {
+function ProductList({ arts }: any) {
+
+  const router = useIonRouter();
+  
+  function handleProductDetail(id:string) {
+    router.push(`/creative/products/${id}`);
+  }
   return (
     <div className='products-list-container'>
       {
         arts.map((art,index) => (
-           <div className="product" key={index}>
+           <div className="product" key={index} onClick={()=>{handleProductDetail(art.id)}}>
         <div className="img">
             <img src={art.product_link} alt="image" />
         </div>
--- src/_creatives/pages/products/UploadProduct.tsx
+++ src/_creatives/pages/products/UploadProduct.tsx
@@ -9,10 +9,26 @@ import {
   save,
   sparklesSharp,
 } from 'ionicons/icons';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+const API_KEY = "AIzaSyBXanBrQOiU9qP1DrSnaic967YB2nJRMrs";
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+type ChatPart = {
+  text: string;
+};
+
+type ChatMessage = {
+  role: "user" | "model";
+  parts: ChatPart[];
+};
+
 
 const UploadProduct: React.FC = () => {
   const [image, setImage] = useState(null);
   const [audioFile, setAudioFile] = useState<File | null>(null);
+  const [ans, setAns] = useState('');
 
   const [videoSrc, setVideoSrc] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
@@ -97,6 +113,50 @@ const UploadProduct: React.FC = () => {
     }
   };
 
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const [question, setQuestion] = useState<string>("");
+  const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]);
+
+
+    const generateAnswer = async () => {
+    const msg = question;
+
+    console.log(question, 'msg', msg)
+    
+    setChatHistory((prevChatHistory) => [
+      ...prevChatHistory,
+      {
+        role: "user",
+        parts: [{ text: question }],
+      },
+    ]);
+
+    try {
+      const chat = model.startChat({
+        history: chatHistory,
+        generationConfig: {
+          maxOutputTokens: 500,
+        },
+      });
+
+      const { response } = await chat.sendMessage(msg);
+      const text = await response.text();
+    
+      setChatHistory((prevChatHistory) => [
+        ...prevChatHistory,
+        {
+          role: "model",
+          parts: [{ text }],
+        },
+      ]);
+      
+      setQuestion(""); 
+       setAns(text);
+    } catch (error) {
+      console.error("Error generating answer:", error);
+    }
+  };
+
   return (
     <IonPage>
       <BackBtn title="Post Your Artwork" />
@@ -173,14 +233,16 @@ const UploadProduct: React.FC = () => {
                   <textarea
                     name=""
                     id=""
+                    value={ans}
                     placeholder="Story behind your art?"
+                    onChange={(e)=>{setAns(e.target.value)}}
                   ></textarea>
                 </div>
 
                 <div className="btn-add-edit-text">
                   <button className="btn-enhance-text">
                     <IonIcon icon={sparklesSharp} />
-                    <span>Enhance with AI</span>
+                    <span onClick={()=>{generateAnswer()}}>Enhance with AI</span>
                   </button>
                   <button className="btn-save-text" onClick={
                     handleNextStep
