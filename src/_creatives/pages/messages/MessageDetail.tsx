129e9b6b901c5cdd9478f9f1fb5af806a6ad82f6
JIM440 <takemjim43@gmail.com>
2024-08-27 10:08:53 +0100

    more changes

--- src/_creatives/pages/messages/MessageDetail.tsx
+++ src/_creatives/pages/messages/MessageDetail.tsx
@@ -1,18 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonFooter, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import BackBtn from '../../../components/BackBtn';
+import { send } from 'ionicons/icons';
 
 const MessageDetails: React.FC = () => {
 
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Page Title</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+        <IonPage className='message-details'>
+            <BackBtn title='Loic Nathan' />
             <IonContent className="ion-padding">
             Message Details
             </IonContent>
+            <IonFooter className='message-footer'>
+                <input type="text" name="" id="" placeholder='Type your message here' />
+                <IonIcon icon={send} />
+            </IonFooter>
         </IonPage>
     );
 };
--- src/components/ArtsWorks.tsx
+++ src/components/ArtsWorks.tsx
@@ -3,43 +3,43 @@ import ArtWorksSkeleton from './skeletons/ArtWorksSkeleton'
 
 function ArtsWorks({ arts }:any) {
     return (
-        // <div className='arts-works-container'>
-        //     {arts.map((art, index) => {
-        //         if (index % 2 === 0) {
-        //             return (
-        //                 <div className="left" key={index}>
-        //                     <div className="arts-works">
-        //                         <div className="img">
-        //                             <img src={art.product_link} alt={art.title} />
-        //                         </div>
-        //                         <div className="content">
-        //                             <h3>{art.author_name}</h3>
-        //                             <p className='ellipses'>{art.product_name}</p>
-        //                             <span>US$ {art.product_price}</span>
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             );
-        //         } else {
-        //             return (
-        //                 <div className="right" key={index}>
-        //                     <div className="arts-works">
-        //                         <div className="img">
-        //                             <img src={art.product_link} alt={art.title} />
-        //                         </div>
-        //                         <div className="content">
-        //                             <h3>{art.author_name}</h3>
-        //                             <p className='ellipses'>{art.product_name}</p>
-        //                             <span>US$ {art.product_price}</span>
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             );
-        //         }
-        //     })}
-        // </div>
+        <div className='arts-works-container'>
+            {arts.map((art, index) => {
+                if (index % 2 === 0) {
+                    return (
+                        <div className="left" key={index}>
+                            <div className="arts-works">
+                                <div className="img">
+                                    <img src={art.product_link} alt={art.title} />
+                                </div>
+                                <div className="content">
+                                    <h3>{art.author_name}</h3>
+                                    <p className='ellipses'>{art.product_name}</p>
+                                    <span>US$ {art.product_price}</span>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div className="right" key={index}>
+                            <div className="arts-works">
+                                <div className="img">
+                                    <img src={art.product_link} alt={art.title} />
+                                </div>
+                                <div className="content">
+                                    <h3>{art.author_name}</h3>
+                                    <p className='ellipses'>{art.product_name}</p>
+                                    <span>US$ {art.product_price}</span>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                }
+            })}
+        </div>
 
-        <ArtWorksSkeleton />
+        // <ArtWorksSkeleton />
     );
 }
 
