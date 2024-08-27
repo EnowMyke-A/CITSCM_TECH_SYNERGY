5bef899d13241007a6e6a0d6fe399e50d98621ce
EnowMyke-A <enowmyke944@gmail.com>
2024-08-27 04:33:51 +0100

    active_tab

--- src/_creatives/pages/home/Home.tsx
+++ src/_creatives/pages/home/Home.tsx
@@ -1,56 +1,72 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import React, { useState, useEffect } from "react";
-import './home.css';
-import ProductList from './components/ProductList';
-import ArtsWorks from '../../../components/ArtsWorks';
-import { getAllProducts } from '../../../services/product';
-import { collection, onSnapshot } from 'firebase/firestore';
-import { db } from '../../../config/firebase';
-
+import "./home.css";
+import ProductList from "./components/ProductList";
+import ArtsWorks from "../../../components/ArtsWorks";
+import { getAllProducts } from "../../../services/product";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../../config/firebase";
 
 const Home: React.FC = () => {
-    const [Arts, setArts] = useState<any[]>([]);
+  const [Arts, setArts] = useState<any[]>([]);
 
-    useEffect(() => {
-        getProducts();
-    }, []); 
+  const [tabButton, setActiveTabButton] = useState("for_you");
 
-    async function getProducts() {
-           const colRef = collection(db, 'Products');
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    const colRef = collection(db, "Products");
     const list: any[] = [];
 
     const unsubscribe = onSnapshot(colRef, (snapshot) => {
-        snapshot.forEach((doc) => {
-            list.push({ id: doc.id, ...doc.data() });
-        });
-        setArts(list)
+      snapshot.forEach((doc) => {
+        list.push({ id: doc.id, ...doc.data() });
+      });
+      setArts(list);
     });
-    }
+  }
 
-    return (
-        <IonPage>
-            <IonContent className="ion-padding">
-                <div className="home-top-section">
-                    <div className="logo">
-                        <img src="" alt="" />
-                    </div>
-                </div>
-                <div className="home-section-1">
-                    <h2>Newest Artworks</h2>
-               
-                    <ProductList arts={ Arts } />
-                     
-                </div>
+  return (
+    <IonPage>
+      <IonContent className="ion-padding">
+        <div className="home-top-section">
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="home-section-1">
+          <h2>Newest Artworks</h2>
 
-                <div className="sticky-top">
-                    <button className='active'>For you</button>
-                    <button>For me</button>
-                </div>
+          <ProductList arts={Arts} />
+        </div>
 
-                <ArtsWorks arts={ Arts} />
-            </IonContent>
-        </IonPage>
-    );
+        <div className="sticky-top">
+          <button
+            className={tabButton == "for_you" ? "active" : ""}
+            onClick={() => setActiveTabButton("for_you")}
+          >
+            For you
+          </button>
+          <button
+            className={tabButton == "for_me" ? "active" : ""}
+            onClick={() => setActiveTabButton("for_me")}
+          >
+            For me
+          </button>
+        </div>
+
+        <ArtsWorks arts={Arts} />
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Home;
--- src/_creatives/pages/home/components/ProductList.tsx
+++ src/_creatives/pages/home/components/ProductList.tsx
@@ -1,33 +1,33 @@
-import { IonIcon } from '@ionic/react'
-import { chevronForward, play } from 'ionicons/icons'
-import React from 'react'
+import { IonIcon } from "@ionic/react";
+import { chevronForward, play } from "ionicons/icons";
+import { useIonRouter } from "@ionic/react";
+import React from "react";
 
-function ProductList({arts}:any) {
+function ProductList({ arts }: any) {
+  const router = useIonRouter();
+
   return (
-    <div className='products-list-container'>
-      {
-        arts.map((art,index) => (
-           <div className="product" key={index}>
-        <div className="img">
+    <div className="products-list-container">
+      {arts.map((art, index) => (
+        <div className="product" key={index}>
+          <div className="img">
             <img src={art.product_link} alt="image" />
-        </div>
-        <div className="content">
+          </div>
+          <div className="content">
             <div>
-            <h3>{ art.author_name}</h3>
-            <p className='ellipses'>{ art.product_name}</p>
-            <p>US$ { art.product_price}</p>
+              <h3>{art.author_name}</h3>
+              <p className="ellipses">{art.product_name}</p>
+              <p>US$ {art.product_price}</p>
             </div>
             <button className="see-story">
-                <span>See story</span>
-                <IonIcon icon={chevronForward} />
+              <span>See story</span>
+              <IonIcon icon={chevronForward} />
             </button>
+          </div>
         </div>
-      </div>
-        ))
-      }
-     
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
